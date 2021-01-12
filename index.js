8e9c16e628a0a141fbe8f47b356aacd2cb919956
bswan0002 <bswan0002@gmail.com>
2021-01-12 14:37:12 -0800

    tests pass

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(nameArr, giftType) {
+  let thanksArr = [];
+  nameArr.forEach((name) => {
+    thanksArr.push(`Thank you, ${name}, for the wonderful ${giftType} gift!`);
+  });
+  return thanksArr;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
